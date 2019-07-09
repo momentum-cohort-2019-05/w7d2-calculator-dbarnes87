7668de4e41028d103df4db5eedbdd8d9fd2050a9
David Barnes <david.barnes115@gmail.com>
2019-07-09 18:33:01 -0400

    fixed eval() functionality

--- main.js
+++ main.js
@@ -11,20 +11,72 @@ window.onload = function() {
     })
 */
 
+const output = document.querySelector(".output")
+const clear = document.querySelector('.clear')
+//const dotButton = document.querySelector('.decimal')
+const dividButton = document.querySelector('.divide')
+const multButton = document.querySelector('.multiply')
+const subButton = document.querySelector('.minus')
+const plusButton = document.querySelector('.plus')
+const equalsign = document.querySelector('.equals')
+
+
+
+
+
+//clears display
 function newFunction() {
     document.getElementById('input').value='';
 }
 
 window.onload = function() {
 
+
+
+
 var buttons = document.querySelectorAll(".numbers")
-//console.log(buttons)
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        if (button.value !== "="){
         document.getElementById('input').value += button.value;
+        } else {
+            const currentValue = output.value
+            output.value = eval(currentValue)
+        }
     });
 });
+dotButton.addEventListener('click', function () {
+    output.value += dotButton.value
+})
+
+dividButton.addEventListener('click', function () {
+    output.value += dividButton.value
+})
+
+plusButton.addEventListener('click', function () {
+    output.value += plusButton.value
+})
+
+multButton.addEventListener('click', function () {
+    output.value += multButton.value
+})
+
+subButton.addEventListener('click', function () {
+    output.value += subButton.value
+})
+
+
+// equalsign.addEventListener('click', function () {
+//    const currentValue = output.value
+//    output.value = eval(currentValue)
+// })
+
+
+
+
 };
+
+
 
 
 
